649ec56c51c1419eacf2c8614baec0783cbf20e5
Kelan Mazey <kelanmazey@gmail.com>
2026-05-05 09:41:46 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,9 +7,20 @@ def main():
 
     # TODO: Uncomment the code below to pass the first stage
     
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    with socket.create_server(("localhost", 4221), reuse_port=True) as server:
+        while True:
+            conn, add = server.accept() # wait for client
 
+            data = conn.recv(16384)
+            if not data:
+                continue
+            else:
+                conn.send(get_status_line("HTTP/1.1", 200, "OK"))
+
+
+
+def get_status_line(protocol: str, code: int, reason_phrase: None | str = None):
+    return f"{protocol} {code} {reason_phrase}\r\n\r\n".encode(encoding="utf-8")
 
 if __name__ == "__main__":
     main()
